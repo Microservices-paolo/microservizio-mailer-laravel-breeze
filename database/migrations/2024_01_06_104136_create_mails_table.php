94362c16de291611ca2e1d10f83e5737b4c208f6
Vincenzo <divitavincenzo@icloud.com>
2024-01-15 16:04:20 +0100

    Commit provvisorio

--- database/migrations/2024_01_06_104136_create_mails_table.php
+++ database/migrations/2024_01_06_104136_create_mails_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('mails', function (Blueprint $table) {
             $table->id();
-            $table->string('mailName');
+            $table->string('mailName')->unique();
             $table->string('mailHost');
-            $table->string('mailUsername');
+            $table->string('mailUsername')->unique();
             $table->string('mailPassword');
             $table->string('mailSmtpSecure');
             $table->string('mailPort');
