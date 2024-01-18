f8a88344581fc8488d3ac4170f808916badd1aee
Paolo Falco <paolo.falcoapp@gmail.com>
2024-01-18 11:31:04 +0100

    mod builder and controller

--- app/Http/Services/PrenotationBuilder.php
+++ app/Http/Services/PrenotationBuilder.php
@@ -6,7 +6,7 @@ class PrenotationBuilder
 {
     // buildMailBody() è il metodo di partenza per generare l'html delle mail, il contenuto del body viene generato dagli altri due metodi
     public function buildMailBody($mail, $name, $telephone, $people, $messagge, $date, $hour, $isClient){
-        $content =  $isClient ? $this->getMailBodyClient($name) : $this->getMailBodyAdmin($mail, $name, $telephone, $people, $messagge, $date, $hour);
+        $content =  $isClient ? $this->getMailBodyClient($name, $people, $date, $hour) : $this->getMailBodyAdmin($mail, $name, $telephone, $people, $messagge, $date, $hour);
         return <<<END
             <!DOCTYPE html>
             <html lang="en">
@@ -23,11 +23,11 @@ class PrenotationBuilder
     }
 
     // getMailBodyAdmin() genera il body della mail di richiesta informazioni che arriverà all'admin proprietario del front-end
-    function getMailBodyAdmin($mail, $name, $telephone, $people, $messagge, $date, $hour) {
+    function getMailBodyAdmin($mail, $name, $telephone, $people, $message, $date, $hour) {
         return <<<END
             <h1>Prenotazione tavolo $name</h1>
             <p>$name ha prenotato un tavolo per $people persone per il giorno $date alle ore $hour.</p>
-            <span>Mail: $mail </span>
+            <span>Mail: $mail, Cellulare: $telephone </span>
             // TO DO ADD IF
             <span>Messaggio da parte del cliente:</span>
             <p>$message</p>
@@ -36,14 +36,14 @@ class PrenotationBuilder
     }
 
     // getMailBodyClient() genera il body della mail di conferma riguardo l'avvenuto invio della richiesta di informazioni presso l'admin. Tale mail arriva quindi al cliente.
-    function getMailBodyClient($name) {
+    function getMailBodyClient($name, $people, $date, $hour) {
         return <<<END
         
             <h1>Ciao $name</h1>
             
             <p>La tua prenotazione è stata inoltrata con successo!</p>
             <span>Riepilogo prenotazione:</span>
-            <p>$nome, $people, $date, $hour</p>
+            <p>$name, $people, $date, $hour</p>
         
         END;
     }
